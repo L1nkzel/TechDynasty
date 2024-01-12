13a9a20654b173ed2f68f23b110062f899d21d07
L1nkzel <92026007+L1nkzel@users.noreply.github.com>
2024-01-12 17:09:08 +0100

    Implemented routing

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -6,10 +6,12 @@ import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
 import Button from "@material-ui/core/Button";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { List, ListItem, ListItemText, Collapse, Box } from "@mui/material";
+import { List, ListItem, ListItemText, Collapse, Box} from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import UserIcon from "@mui/icons-material/Person";
 import Colors from "../assets/styles/Colors";
+import { Link } from "react-router-dom";
+
 
 // Using Inline Styling
 const useStyles = makeStyles(() => ({
@@ -36,52 +38,59 @@ export default function NavBar() {
   };
 
   return (
-      <AppBar className={classes.root}>
-        <Toolbar>
-          {small && (
-            <Box flex={1}>
-              <Box display={"flex"}>
-                <Typography variant="h6" color="inherit" component={"div"}>
-                  Tech Dynasty
-                </Typography>
-                <Box className={classes.menuButton}>
-                  <Button style={{color:"white"}} onClick={handleClick}>
-                    <MenuIcon />
-                  </Button>
-                </Box>
+    <AppBar className={classes.root}>
+      <Toolbar>
+        {small && (
+          <Box flex={1}>
+            <Box display={"flex"}>
+
+              <Typography variant="h6" color="inherit" component={"div"}>
+                Tech Dynasty
+              </Typography>
+              <Box className={classes.menuButton}>
+                <Button style={{ color: "white" }} onClick={handleClick}>
+                  <MenuIcon />
+                </Button>
               </Box>
-              <Collapse in={open} timeout="auto">
-                <List component="div" disablePadding>
+            </Box>
+            <Collapse in={open} timeout="auto">
+              <List component="div" disablePadding>
+                <Link to="/" style={{color: "white", textDecoration: "none"}}>
                   <ListItem>
                     <ListItemText primary="Cart" />
-                  </ListItem>{" "}
+                  </ListItem>
+                </Link>
+                <Link to="/" style={{color: "white", textDecoration: "none"}}>
                   <ListItem>
                     <ListItemText primary="Log in" />
                   </ListItem>
-                </List>
-              </Collapse>
-            </Box>
-          )}
+                </Link>
+              </List>
+            </Collapse>
+          </Box>
+        )}
 
-          {full && (
-            <Box display="flex" flexGrow={1}>
-              <Typography variant="h6" color="inherit">
-                Tech Dynasty
-              </Typography>
-              <Box display="flex" justifyContent="end" flexGrow={1}>
-                <Button color="inherit">
-                  <ShoppingCartIcon />
-                  Cart
-                </Button>
-                <Button color="inherit">
-                  <UserIcon />
-                  Log in
-                </Button>
-              </Box>
+        {full && (
+          <Box display="flex" flexGrow={1}>
+            <Link to="/" style={{color: "white", textDecoration: "none"}}>
+            <Typography variant="h6">
+              Tech Dynasty
+            </Typography>
+            </Link>
+            <Box display="flex" justifyContent="end" flexGrow={1}>
+              <Button href="/" color="inherit">
+                <ShoppingCartIcon />
+                Cart
+              </Button>
+
+              <Button href="/" color="inherit">
+                <UserIcon />
+                Log in
+              </Button>
             </Box>
-          )}
-        </Toolbar>
-      </AppBar>
- 
+          </Box>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 }
