a29d7de59db47a343214091988d3d63623f6cad7
L1nkzel <purehoneygaming@proton.me>
2024-01-13 19:21:20 +0100

    Created Product details page

--- frontend/src/components/Rating.tsx
+++ frontend/src/components/Rating.tsx
@@ -6,27 +6,29 @@ import { FunctionComponent } from "react";
 
 interface RatingProps {
   value: number;
-  text: number;
+  text: string;
+  iconFontSize: number;
+
 }
 
 const ratingArray = [1, 2, 3, 4, 5];
 
 
-const Rating: FunctionComponent<RatingProps> = ({ value, text }) => {
+const Rating: FunctionComponent<RatingProps> = ({ value, text, iconFontSize }) => {
   return (
     <Box display="flex" flexWrap="wrap" alignItems="center" my={0.5}>
       {ratingArray.map((index) => (
         <Box  key={index} color={"gold"}>
           {value >= index ? (
-            <StarIcon style={{fontSize: 20}}/>
+            <StarIcon style={{fontSize: iconFontSize}}/>
           ) : value >= index - 0.5 ? (
-            <StarHalfIcon style={{fontSize: 20}}/>
+            <StarHalfIcon style={{fontSize: iconFontSize}}/>
           ) : (
-            <StarBorderIcon style={{fontSize: 20}}/>
+            <StarBorderIcon style={{fontSize: iconFontSize}}/>
           )}
         </Box>
       ))}
-      <Typography fontSize={15} ml={0.5}>{text && text} reviews</Typography>
+      <Typography fontSize={15} ml={0.5}>{text && text}</Typography>
     </Box>
   );
 };
