c69e10ebde14d6c759827bcc9cbe3a3c48e77e73
L1nkzel <92026007+L1nkzel@users.noreply.github.com>
2024-01-17 15:51:30 +0100

    Implemented Error middleware and product controller

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db";
 import productRoutes from "./routes/productRoutes";
+import { errorHandler, notFoundHandler } from "./middleware/errorMiddleware";
 
 const port = process.env.PORT || 5000;
 
@@ -17,6 +18,8 @@ app.get("/", (req, res) => {
 // Routes 
 app.use('/api/products/', productRoutes);
 
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
