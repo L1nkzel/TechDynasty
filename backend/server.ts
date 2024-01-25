8ea3e3911f4b9e2d8ac0886214b4c5c806d80d36
L1nkzel <purehoneygaming@proton.me>
2024-01-25 19:10:25 +0100

    Added user authentication and profile routes, user model with password hashing

--- backend/server.ts
+++ backend/server.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db";
 import productRoutes from "./routes/productRoutes";
+import userRoutes from "./routes/userRoutes";
 import { errorHandler, notFoundHandler } from "./middleware/errorMiddleware";
 
 const port = process.env.PORT || 5000;
@@ -11,13 +12,19 @@ connectDB(); // Connect to MongoDB
 
 const app = express();
 
-app.get("/", (req, res) => {
+// Middleware
+app.use(express.json()); // Parse JSON request bodies 
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies 
+
+app.get("/", (req, res) => { // Test route 
   res.send("Api is running...");
 });
 
 // Routes 
 app.use('/api/products/', productRoutes);
+app.use('/api/users/', userRoutes);
 
+// Error handlers 
 app.use(notFoundHandler);
 app.use(errorHandler);
 
