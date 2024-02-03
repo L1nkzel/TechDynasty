9925a4e73e05418ba8c3274e559945e86d01f3ac
L1nkzel <92026007+L1nkzel@users.noreply.github.com>
2024-02-03 17:33:34 +0100

    auth, userApi and loginRegisterSlices

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
 import shoppingCartSlice from './slices/shoppingCartSlice';
+import authSlice from './slices/authSlice';
+import loginRegisterSlice from './slices/loginRegisterSlice';
 
 const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
         shoppingCart: shoppingCartSlice,
+        auth: authSlice,
+        loginRegister: loginRegisterSlice
     },
     middleware: (getDefaultMiddleware) => 
     getDefaultMiddleware().concat(apiSlice.middleware),
